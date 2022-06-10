058d939887255ab25d11b39fae02caabfcaa4f85
Nicholas Esposito <nesposito@ufl.edu>
2022-06-10 11:44:45 -0400

    Updated lecture 4 example

--- 04-Ohtani-Batting-Visualiztion-2.R
+++ 04-Ohtani-Batting-Visualiztion-2.R
@@ -67,3 +67,50 @@ table("Pitch Type" = ohtani_batted_balls$pitch_type,
       "Batted Ball Type" = ohtani_batted_balls$batted_ball_type) %>%
   mosaicplot(main = "Relationship between batted ball type and pitch type?",
              shade = TRUE) # Shade by Pearson residuals (how far we differ from exp. independence)
+
+# Continuous by categorical visualization ---------------------------------
+
+# Side-by-side
+ohtani_batted_balls %>%
+  ggplot(aes(x=pitch_type, y=exit_velocity)) +
+  geom_violin() +
+  geom_boxplot(width = 0.25) +
+  theme_bw()
+
+# Color - Gaps indicate differences in distributions
+ohtani_batted_balls %>%
+  ggplot(aes(x=exit_velocity, color = pitch_type)) +
+  stat_ecdf() +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+# Histogram (Stacked, hard to see conditional dist.)
+ohtani_batted_balls %>%
+  ggplot(aes(x=exit_velocity, fill = pitch_type)) +
+  geom_histogram() +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+# Histogram (Change transparency and position, hard to differentiate colors)
+ohtani_batted_balls %>%
+  ggplot(aes(x=exit_velocity, fill = pitch_type)) +
+  geom_histogram(alpha = 0.25, position = "identity") +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+# Histogram (Break up into subplots with facet wrap)
+ohtani_batted_balls %>%
+  ggplot(aes(x=exit_velocity)) +
+  geom_histogram() +
+  theme_bw() +
+  facet_wrap(~ pitch_type, 
+             ncol = 2,
+             scales = "free_y") # Break into subplots with free y-axes
+
+# Histogram (Stack subplots with facet grid)
+ohtani_batted_balls %>%
+  ggplot(aes(x=exit_velocity)) +
+  geom_histogram() +
+  theme_bw() +
+  facet_grid(pitch_type ~.,
+             margins = TRUE) # Stack the subplots
