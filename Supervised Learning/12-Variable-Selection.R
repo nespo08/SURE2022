c99636b0d7a2125ede97ff1a11c2439db461b47a
Nicholas Esposito <nesposito@ufl.edu>
2022-06-29 11:54:37 -0400

    Lecture 12

--- Supervised Learning/12-Variable-Selection.R
+++ Supervised Learning/12-Variable-Selection.R
@@ -65,3 +65,67 @@ ggpairs(nfl_model_data,
         aes(alpha = 0.25)) +
   theme_bw()
 
+
+# Perform 5-fold cross validation -----------------------------------------
+
+set.seed(2020)
+nfl_model_data <- nfl_model_data %>%
+  mutate(test_fold = sample(rep(1:5, 
+                                length.out = n())))
+
+# Get cross-validation predictions
+# INPUTS: model_formula, dataset (where dataset assumed to have test_fold)
+# OUTPUTS: dataset containing test fold predictions
+
+get_cv_preds <- function(model_formula, dataset = nfl_model_data) {
+  
+  map_dfr(unique(dataset$test_fold),
+          function(holdout_i){
+            
+            # Separate and get the training and test data
+            test_data <- dataset %>%
+              filter(test_fold == holdout_i)
+            
+            train_data <- dataset %>%
+              filter(test_fold != holdout_i)
+            
+            # Fit the model on training data
+            train_model <- 
+              lm(as.formula(model_formula),
+                 data = train_data)
+            
+            # Return a table of holdout predictions
+            tibble(test_preds = predict(train_model, newdata = test_data),
+                   test_actual = test_data$score_diff,
+                   test_fold = holdout_i)
+          })
+}
+
+# Get cross-validation results for different sets of variables
+all_cv_preds <- get_cv_preds("score_diff ~  offense_ave_epa_pass + offense_ave_epa_run + defense_ave_epa_pass + defense_ave_epa_run")
+all_int_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_pass*offense_ave_epa_run + defense_ave_epa_pass*defense_ave_epa_run")
+run_only_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_run + defense_ave_epa_run")
+pass_only_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_pass + defense_ave_epa_pass")
+off_only_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_pass + offense_ave_epa_run")
+def_only_cv_preds <- get_cv_preds("score_diff ~ defense_ave_epa_pass + defense_ave_epa_run")
+int_only_cv_preds <- get_cv_preds("score_diff ~ 1") # Baseline, no variables used
+
+# Plot
+bind_rows(mutate(all_cv_preds, type = "All"),
+          mutate(all_int_cv_preds, type = "All w/ interactions"),
+          mutate(pass_only_cv_preds, type = "Passing only"),
+          mutate(run_only_cv_preds, type = "Running only"),
+          mutate(off_only_cv_preds, type = "Offense only"),
+          mutate(def_only_cv_preds, type = "Defense only"),
+          mutate(int_only_cv_preds, type = "Intercept-only")) %>%
+  group_by(type, test_fold) %>%
+  summarize(rmse = sqrt(mean((test_actual - test_preds)^2))) %>%
+  mutate(type = fct_reorder(type, rmse)) %>%
+  ggplot(aes(x = type, y = rmse)) +
+  geom_point(alpha = 0.5) +
+  theme_bw() +
+  coord_flip()
+
+# From here, we decide which variables to use with our model (look for lowest MSE)
+all_lm <- lm(score_diff ~ offense_ave_epa_pass + offense_ave_epa_run + defense_ave_epa_pass + defense_ave_epa_run, data = nfl_model_data)
+summary(all_lm)
